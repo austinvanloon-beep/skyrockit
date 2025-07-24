cd957097cc03e61729549cad0db6b28c17ab8269
Austin Vanloon <wic404learn@gmail.com>
2025-07-24 14:41:58 -0600

    attempted full crud utilization

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,24 +3,21 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-router.get('/', (req, res) => {
-    try {
-        const currentUser = await User.findById(req.sessionStore.user._id.)
-        res.render('applications/index.ejs', {
-        applications: currentUser.applications,
-        })
-    } catch (error) {
-        console.log(error)
-            res.redirect('/')
-        }
-    })
-
 
 // GET route for /new
 
-router.get('/new', async (req, res) => {
-    res.render('applications/new.ejs')
+router.get('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id) // also remove that extra period after _id
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
 })
+
 
 
 // POST fpr /applictions
@@ -37,5 +34,65 @@ router.post('/', async (req, res) => {
 })
 
 
+// GET to /applications/:appliucationId
+router.get('/:applicationId', (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/show.ejs', {
+            application: application,
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+
+
+// DELETE /application/:applicationId
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.applications.id(req.params.applicationId).deleteOne()
+        currentUser.save()
+        res.redirect(`/users/${currentUserUser._id}/applications`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+
+// controllers/applications.js
+
+router.get('/:applicationId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/edit.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+
+router.put('/:applicationId', async (req, res) => => {
+    try {
+        const currentUser = await User.finById(req.session.user._id)
+        const application = currentUser.applications.id(req.params.applicationId)
+        application.set(req.body)
+        await currentUser.save()
+
+        res.redirect(`/users/${currentUser._id}/applications${req.params.applicationId}`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
 
 module.exports = router
